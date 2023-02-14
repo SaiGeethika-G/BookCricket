6c3869fdd13133cfeac0127a056d90cb0f4359cb
Sai Geethika Gudapati <79441278+SaiGeethika-G@users.noreply.github.com>
2023-02-14 16:57:47 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCricket
+{
+    public class Class1
+    {
+        public void playGame()
+        {
+            int totalRuns = 0;
+            string reqString = "Please press enter to score!";
+            int ballsForOver = 6;
+            bool outOfGame=false;
+            for (int i = 0; i < ballsForOver; i++)
+            {
+                Console.Write(reqString);
+                string? userEntry = Console.ReadLine();
+                if (userEntry == "")
+                {
+                    int currentPageScore = runsScored();
+                    if (currentPageScore == 0)
+                    {
+                        Console.WriteLine("Out!!!");
+                        outOfGame= true;
+                        break;
+                    }
+                    else if (currentPageScore > 6)
+                    {
+                        totalRuns += 0;
+                    }
+                    else
+                    {
+                        totalRuns += currentPageScore;
+                    }
+                }
+            }
+            if (!outOfGame)
+            {
+                Console.WriteLine("Game Over! The total score is {0}", totalRuns);
+            } 
+        }
+        int runsScored()
+        {
+            int totalPages = 500;
+            Random randObj = new Random();
+            int page = randObj.Next(0, totalPages);
+            Console.WriteLine("Current page: {0}\n", page);
+            int currentPageScore = page % 10;
+            return currentPageScore;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BookCricket;
+
+Class1 newGame= new Class1();
+newGame.playGame();
